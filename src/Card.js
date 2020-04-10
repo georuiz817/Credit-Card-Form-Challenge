06664b3e3d568198786f732895f90fb08e7e26cc
georuiz817 <49955820+georuiz817@users.noreply.github.com>
2020-04-10 15:21:04 -0400

    first

---
+++ src/Card.js
@@ -0,0 +1,34 @@
+import React  from 'react';
+import { Col, Row} from 'react-bootstrap';
+
+
+const Card = () => {
+
+  //const [Memos, setMemos] = useState([]);
+
+   return (
+         <div>
+             <Row>
+                 <Col>
+                    img
+                 </Col>
+                 <Col>
+                    img
+                 </Col>
+             </Row>
+             <Row>
+                 <Col>
+                    <input></input>
+                 </Col>
+             </Row>
+             <Row>
+                 <Col>
+                    CardHolder
+                 </Col>
+             </Row>
+             
+         </div>
+            )
+          }
+
+export default Card
