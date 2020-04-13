5ffc902cc8b1cc0fe0e4b2ea00644fbe8be8c61f
georuiz817 <49955820+georuiz817@users.noreply.github.com>
2020-04-12 23:36:50 -0400

    card component set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import CreditBox from './CreditBox';
 import './index.css';
- 
+import Card from './Card'
 
 function App() {
   return (
     <div className="App">
+      <Card />
       <CreditBox />
     </div>
   );
---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React  from 'react';
+
+ 
+const Card = () => {
+
+
+   return (
+    <div>
+        <p>George Ruiz</p>
+        <p id='CardNum'>3456-3445-2243-2323</p>
+        <p>04/20</p>
+    </div>
+    )
+  }
+
+export default Card
